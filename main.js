e42027748e97e52341627c3dd90a845aa80f6da4
Brandon Buchs <brakebu@pop-os.localdomain>
2020-02-27 22:27:33 -0500

    Completed Project

--- main.js
+++ main.js
@@ -1,30 +1,52 @@
-var turf = require('@turf/turf');
-var fs = require('fs');
-var wells = fs.readFileSync('./data/wells.geojson');
-var tracts = fs.readFileSync('./data/tracts.geojson');
+/*//global variables
+const http = require('http');
+const fs = require('fs');
 
-wells = JSON.parse(wells);
-tracts = JSON.parse(tracts);
-var wells_options = {gridType: 'hex', property: 'nitr_con', units: 'miles'}
-var idw_wells = turf.interpolate(wells,5,wells_options);
-fs.writeFileSync('./data/idw_wells.geojson', JSON.stringify(idw_wells));
+const hostname = '127.0.0.1';
+const port = '3000';
 
+fs.readFile('index.html', (err, html) =>{
+    if(err){
+        throw err;
+    }
 
-//calculate mean area for census tracts
-var tracts_area = [];
-var tracts_centroid = [];
-console.log(tracts.features.length);
-for (var i = 0; i < tracts.features.length; i++) {
-    var canrate = tracts.features[i].canrate;
-    tracts_area.push(turf.area(tracts.features[i]));
-    tracts_centroid.push(turf.centroid(tracts.features[i], {'canrate': tracts.features[i].properties.canrate}));
+    const server = http.createServer((req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-type', 'text/html');
+        res.write(html);
+        res.end();
+    });
+    
+    server.listen(port, hostname, () => {
+        console.log('Server started on port ' +port);
+    });
+});*/
+
+var numbers = [9, 2, 5, 4, 12, 7];
+var numbersTwo = [1, 2, 3, 4, 5, 6, 7, 8];
+
+function standardDeviation (n) {
+    var sum = 0;
+    var sumDiff = 0;
+    for (i = 0; i < n.length; i++) {
+        sum += n[i];
+    }
+    var avg = sum / n.length;
+    console.log(avg);
+    for (i = 0; i < n.length; i++) {
+        console.log(sumDiff);
+        sumDiff += (n[i] - avg)**2
+        console.log(sumDiff);
+    }
+    
+    var stdDeviation = (1/(n.length - 1)*sumDiff)**(1/2);
+    console.log(stdDeviation);
+    for (i = 0; i < n.length; i++) {
+    }
 }
-console.log(tracts_area.length);
-console.log(tracts_centroid[199]);
-fs.writeFileSync('./data/centroids_tracts.geojson', JSON.stringify(tracts_centroid));
-console.log('saved centroid_tracts.geojson');
 
+standardDeviation(numbers);
+standardDeviation(numbersTwo);
 
-console.log('saved idw_wells.geojson');
 
 
